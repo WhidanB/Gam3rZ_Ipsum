3e3791a832f89922d50fdcfc665422a80879d14a
WhidanB <danyphone95@gmail.com>
2023-06-13 16:53:31 +0200

    removed useless clg 	modified:   modal.js

--- modal.js
+++ modal.js
@@ -3,12 +3,10 @@ const myModal = document.getElementById("exampleModal");
 const btnSuppr = document.querySelectorAll(".delBtn");
 const confirmDel = document.querySelector(".confirmDel");
 let id;
-console.log(btnSuppr);
 
 btnSuppr.forEach((e) =>
   e.addEventListener("click", () => {
     id = e.getAttribute("data-id").valueOf();
-    console.log(id);
     $("#exampleModal").modal("show");
   })
 );
