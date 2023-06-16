62d2134c05478290a80400643477fdc0bdf69219
WhidanB <danyphone95@gmail.com>
2023-06-16 11:18:21 +0200

    fixed responsiveness game page

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
     <link rel="stylesheet" href="./fonts/stylesheet.css">
     <link rel="stylesheet" href="style.css">
 
