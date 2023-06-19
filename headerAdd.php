6430bb5872f59d485422a1d364ce064f545311b3
WhidanB <danyphone95@gmail.com>
2023-06-19 14:22:26 +0200

    fix gallerie screens

--- headerAdd.php
+++ headerAdd.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $title ?></title>    
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="imggallery.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
 
